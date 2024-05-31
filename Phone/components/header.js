0a0edad0d31137d25b87d9fc8ebbde001ad7cfbc
Elias Reis <elias.reis@gov.cv>
2024-05-31 10:00:40 -0100

    updates

--- Phone/components/header.js
+++ Phone/components/header.js
@@ -45,6 +45,13 @@ class Header extends HTMLElement {
           <div class="collapse navbar-collapse p-2" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item pt-2">
+                <a class="nav-link text-white menu-item" href="/about_us">
+                  <h7>
+                    <span>Sobre Nós</span>
+                  </h7>
+                </a>
+              </li>
+              <li class="nav-item pt-2">
                 <a class="nav-link text-white menu-item" href="/solution">
                   <h7>
                     <span>Solução</span>
@@ -53,7 +60,7 @@ class Header extends HTMLElement {
               </li>
               <li class="nav-item pt-2">
                 <a class="nav-link text-white menu-item" href="/business"
-                  ><h7>Negócios</h7></a
+                  ><h7>Preço</h7></a
                 >
               </li>
               <li class="nav-item pt-2 dropdown">
--- index.tsx
+++ index.tsx
@@ -27,6 +27,9 @@ app.get('/faqs',(req:Request, res: Response) => {
 app.get('/tech_corner',(req:Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'pages/tech_corner.html'));
 });
+app.get('/about_us',(req:Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'pages/about_us.html'));
+});
 app.get('/redirect',(req:Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'pages/redirect.html'));
 });
